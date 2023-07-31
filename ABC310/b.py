c4ffa2463c4b003be76adddd4b61afd9c44329de
khirooka <l.00.kocha.00.l@gmail.com>
2023-07-31 15:16:14 +0900

    7/31 310B syakyo

--- ABC310/b.py
+++ ABC310/b.py
@@ -19,22 +19,40 @@
 # print('Yes' if ans else 'No')
 
 """
-7/18(火)
+7/31(月)
 """
 
-N, M = map(int, input().split()) # map(callable, iterable, *iterables) 返り値: function を iterable の全ての要素に適用して結果を生成 (yield) するイテレータ
-
+N, M = map(int, input().split())
 P, C, F = [], [], []
 for i in range(N):
     l = list(map(int, input().split()))
-    P.append(l[0]);
-    C.append(l[1]);
-    F.append(set(l[2:]));
-
+    P.append(l[0])
+    C.append(l[1])
+    F.append(set(l[2:]))
 ans = False
 for i in range(N):
     for j in range(N):
-        ans |= P[i] >= P[j] and F[j].issuperset(F[i]) \
-            and (P[i] > P[j] or C[j] > C[i])
+        ans |= P[i] >= P[j] and F[j].issuperset(F[i]) and (P[i] > P[j] or C[j] > C[i])
+print("Yes" if ans else "No")
 
-print('Yes' if ans else 'No')
+
+"""
+7/18(火)
+"""
+
+# N, M = map(int, input().split()) # map(callable, iterable, *iterables) 返り値: function を iterable の全ての要素に適用して結果を生成 (yield) するイテレータ
+#
+# P, C, F = [], [], []
+# for i in range(N):
+#     l = list(map(int, input().split()))
+#     P.append(l[0]);
+#     C.append(l[1]);
+#     F.append(set(l[2:]));
+#
+# ans = False:
+# for i in range(N):
+#     for j in range(N):
+#         ans |= P[i] >= P[j] and F[j].issuperset(F[i]) \
+#             and (P[i] > P[j] or C[j] > C[i])
+#
+# print('Yes' if ans else 'No')
